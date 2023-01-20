f12aa4a7ca728a5b2994f9069d54587351ac68b6
samgamez <samgamez91@gmail.com>
2023-01-20 00:42:40 -0700

    Implemented bootstrap styles Started logic for tracking and untracking teams

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -14,6 +15,7 @@ import { TeamsService } from './score-tracker/teams.service';
     ScoreTrackerComponent
   ],
   imports: [
+	FormsModule,
     BrowserModule,
     AppRoutingModule,
 	CommonModule,
--- src/app/score-tracker/score-tracker.component.ts
+++ src/app/score-tracker/score-tracker.component.ts
@@ -10,13 +10,29 @@ import { TeamsService } from './teams.service';
 export class ScoreTrackerComponent implements OnInit {
 	teamOptions: Team[] = [];
 
+	trackedTeams: Team[] = [];
+
+	selectedTeamId?: Team;
+
 	constructor(private teamsService: TeamsService) { }
 	ngOnInit(): void {
 		this.teamsService.teams()
 		.subscribe(result => {
 			this.teamOptions = result;
+
+			console.log(this.teamOptions);
 		});
 	}
 
-	
+	trackTeam(team?: Team): void {
+		if (team && !this.trackedTeams.includes(team)) {
+			this.trackedTeams.push(team);
+		}
+	}
+
+	removeTrackedTeam(team: Team){
+		if (this.trackedTeams.includes(team)){
+			this.trackedTeams.splice(this.trackedTeams.indexOf(team), 1);
+		}
+	}
 }
